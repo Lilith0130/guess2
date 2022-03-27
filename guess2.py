fe3018d1b7adad53a757ca71661c79962835065f
LILITHl <Saosao007888@gmail.com>
2022-03-27 18:03:41 +0800

    revise start, end, conut

--- guess2.py
+++ guess2.py
@@ -4,19 +4,23 @@
 #猜錯 要告訴他 比答案大/小
 
 import random 
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end) 
 
-r = random.randint(1, 100)
-
+r = random.randint(start, end )
+count = 0
 while True:
+	count += 1  #count = count + 1 
 	num = input('請輸入數字:')
 	num = int(num)
 	if num == r:
 		print('你猜中!')
-		
+		print('這是你猜的第', count, '次')
 		break
-
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')	
-	
+	print('這是你猜的第', count, '次')
